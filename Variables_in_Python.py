a7146e72da3f63ae140498f39afb24b5bd3377e4
JAI PRAKASH KUMAR <67636249+JPK10125@users.noreply.github.com>
2021-09-28 14:42:11 +0530

    Create Variables_in_Python.py

---
+++ Variables_in_Python.py
@@ -0,0 +1,17 @@
+var1 = "Hello World "
+var2 = 4
+var3 = 36.7
+var4 = "Harry"
+var5 = "32"
+var6 = "54"
+print(var1)
+print(type(var1))  #type is a function which prints the type of the variable
+print(type(var2))  
+print(type(var3))
+print(type(var5))   #var5 is string becouse 32 is written in ("")
+print(var2+var3)   #this will add two int variables, both are integer and float
+#print(var1+var2)   #this will show an error becouse we can not add an string with an integer
+print(var1+var4)   #this will add the two strings, both are strings
+print(var1+var5)   #it will not give an error becouse var1 and var5 are string (var5 is string becouse of "")
+print(var5+var6)   #it will print 32 and 54 like a string
+
